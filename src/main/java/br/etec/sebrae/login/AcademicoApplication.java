1433ac644fcd7507d62e71504f2c2863f5bfbb54
Francisco Aurizelio <francisco.souza63@etec.sp.gov.br>
2021-04-07 20:24:54 -0300

    alteração do pom

---
+++ src/main/java/br/etec/sebrae/login/AcademicoApplication.java
@@ -0,0 +1,13 @@
+package br.etec.sebrae.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/etec/sebrae/academico/AcademicoApplicationTests.java
@@ -0,0 +1,14 @@
+/*package br.etec.sebrae.academico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicoApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
+*/
